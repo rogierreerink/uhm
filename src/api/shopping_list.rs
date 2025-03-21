d515695f590ed2368e8120243a871d7ab7a35da5
Rogier Reerink <me@rogier.io>
2025-03-21 16:47:40 +0100

    Set access control headers on shopping list responses.

--- src/api/shopping_list.rs
+++ src/api/shopping_list.rs
@@ -2,10 +2,13 @@ use crate::api::handle_options;
 use crate::global::AppState;
 
 use axum::{
+    http::{header, HeaderValue},
     routing::{delete, get, options, patch, post},
     Router,
 };
 use std::sync::Arc;
+use tower::ServiceBuilder;
+use tower_http::set_header::SetResponseHeaderLayer;
 
 mod collection_get;
 mod collection_post;
@@ -20,6 +23,17 @@ pub fn create_router(state: Arc<AppState>) -> Router {
                 .route("/", get(collection_get::handle))
                 .route("/", post(collection_post::handle))
                 .route("/", options(handle_options))
+                .layer(
+                    ServiceBuilder::new()
+                        .layer(SetResponseHeaderLayer::if_not_present(
+                            header::ACCESS_CONTROL_ALLOW_METHODS,
+                            HeaderValue::from_static("GET, POST, OPTIONS"),
+                        ))
+                        .layer(SetResponseHeaderLayer::if_not_present(
+                            header::ACCESS_CONTROL_ALLOW_HEADERS,
+                            HeaderValue::from_static("content-type"),
+                        )),
+                )
                 .with_state(state.clone()),
         )
         .merge(
@@ -28,6 +42,17 @@ pub fn create_router(state: Arc<AppState>) -> Router {
                 .route("/:id", patch(resource_patch::handle))
                 .route("/:id", delete(resource_delete::handle))
                 .route("/:id", options(handle_options))
+                .layer(
+                    ServiceBuilder::new()
+                        .layer(SetResponseHeaderLayer::if_not_present(
+                            header::ACCESS_CONTROL_ALLOW_METHODS,
+                            HeaderValue::from_static("GET, PATCH, DELETE, OPTIONS"),
+                        ))
+                        .layer(SetResponseHeaderLayer::if_not_present(
+                            header::ACCESS_CONTROL_ALLOW_HEADERS,
+                            HeaderValue::from_static("content-type"),
+                        )),
+                )
                 .with_state(state.clone()),
         )
 }
